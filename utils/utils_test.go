4d39157816b5d748f187baebe7ee09c30abe2d90
Carlos Derich <d.erich@hotmail.com>
2017-12-24 19:12:37 -0500

    add test for utils

--- utils/utils_test.go
+++ utils/utils_test.go
@@ -36,3 +36,17 @@ func TestPrepareCommand(t *testing.T) {
 func TestPrepareImage(t *testing.T) {
 	assert.Equal(t, PrepareImage("golang", "1.4"), "golang:1.4")
 }
+
+func TestPrepareBeforeCommands(t *testing.T) {
+
+	t.Run("simple single command", func(t *testing.T) {
+		c := PrepareBeforeCommands([]string{"apt-get update"})
+		assert.Equal(t, c, []string{"bash", "-c", "apt-get update"})
+	})
+
+	t.Run("multiple commands", func(t *testing.T) {
+
+		c := PrepareBeforeCommands([]string{"apt-get update", "echo test", "ls"})
+		assert.Equal(t, c, []string{"bash", "-c", "apt-get update && echo test && ls"})
+	})
+}
